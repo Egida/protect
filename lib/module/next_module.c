21b141e21bc01e1cfd110d2aac0eb46927494e60
Glenn Fiedler <glenn@networknext.com>
2025-10-31 08:42:28 -0400

    wip on ed25519

--- lib/module/next_module.c
+++ lib/module/next_module.c
@@ -167,7 +167,7 @@ __bpf_kfunc int bpf_next_sha256( void * data, int data__sz, void * output, int o
 
 __bpf_kfunc int bpf_next_ed25519( void * data, int data__sz, void * output, int output__sz )
 {
-    ed25519_hash( data, data__sz, output );
+    ed25519_verify( data, data__sz, output );
     return 0;
 }
 
