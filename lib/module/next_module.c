f840edec6bda1ab8a05e91231e7a9619f3274f4f
Glenn Fiedler <glenn@networknext.com>
2025-11-04 17:50:22 -0500

    stuff

--- lib/module/next_module.c
+++ lib/module/next_module.c
@@ -27,19 +27,6 @@
 
 // ----------------------------------------------------------------------------------------------------------------------
 
-#define NEXT_SIGN_PUBLIC_KEY_BYTES 32
-
-__bpf_kfunc int bpf_next_sha256( void * data, int data__sz, void * output, int output__sz );
-
-struct next_sign_verify_args
-{
-    __u8 public_key[NEXT_SIGN_PUBLIC_KEY_BYTES];
-};
-
-__bpf_kfunc int bpf_next_sign_verify( void * data, int data__sz, void * signature, int signature__sz, struct next_sign_verify_args * args );
-
-// ----------------------------------------------------------------------------------------------------------------------
-
 struct crypto_shash * sha256;
 
 __bpf_kfunc int bpf_next_sha256( void * data, int data__sz, void * output, int output__sz )
