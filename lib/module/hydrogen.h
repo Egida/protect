f249cc7a5061eb36c3af1722a833a02dd524cba8
Glenn Fiedler <glenn@networknext.com>
2025-11-04 10:24:12 -0500

    again

--- lib/module/hydrogen.h
+++ lib/module/hydrogen.h
@@ -31,7 +31,7 @@ typedef __u64 uint64_t;
 typedef __u32 uint32_t;
 typedef __u16 uint16_t;
 typedef __u8  uint8_t;
-typedef unsigned long long size_t;
+typedef long long unsigned int size_t;
 #define NULL 0
 #endif
 
