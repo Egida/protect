b82715103bf228d9f091d32db0066a6cb714be09
Glenn Fiedler <glenn@networknext.com>
2025-11-23 10:04:58 -0500

    add future

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo ethtool -L enp8s0f0 combined 8
-echo 256 | sudo tee /sys/class/net/enp8s0f0/busy_budget
 echo 2 | sudo tee /sys/class/net/enp8s0f0/napi_defer_hard_irqs
 echo 200000 | sudo tee /sys/class/net/enp8s0f0/gro_flush_timeout
