d43fc882581a14eb12c3cd5a478dd98d9758cc10
Glenn Fiedler <glenn@networknext.com>
2025-11-26 16:59:42 -0500

    aggressive busy poll

--- sdk/next_server_linux.cpp
+++ sdk/next_server_linux.cpp
@@ -1362,7 +1362,7 @@ static void xdp_send_thread_function( void * data )
 
     while ( true )
     {
-        int poll_result = poll( fds, 1, 1 );
+        int poll_result = poll( fds, 1, 0 );
         if ( poll_result < 0 ) 
         {
             next_error( "poll error on socket send queue %d (%d)", socket->queue, poll_result );
@@ -1544,7 +1544,7 @@ static void xdp_receive_thread_function( void * data )
 
     while ( true )
     {
-        int poll_result = poll( fds, 2, -1 );
+        int poll_result = poll( fds, 2, 0 );
         if ( poll_result < 0 ) 
         {
             next_error( "poll error on socket receive queue %d (%d)", socket->queue, poll_result );
