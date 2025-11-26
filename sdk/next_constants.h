cdbe5715c7e4bb99ae4dbb835903cb0488e42348
Glenn Fiedler <glenn@networknext.com>
2025-11-26 17:18:32 -0500

    go

--- sdk/next_constants.h
+++ sdk/next_constants.h
@@ -16,12 +16,12 @@
 
 #ifdef __linux__
 
-#define NEXT_XDP_NUM_FRAMES                                         32768
+#define NEXT_XDP_NUM_FRAMES                                         16384
 #define NEXT_XDP_FRAME_SIZE                                          2048
-#define NEXT_XDP_SEND_QUEUE_SIZE                                     8192
-#define NEXT_XDP_RECV_QUEUE_SIZE                                     8192
+#define NEXT_XDP_SEND_QUEUE_SIZE                                     4096
+#define NEXT_XDP_RECV_QUEUE_SIZE                                     4096
 #define NEXT_XDP_FILL_QUEUE_SIZE                                     2048
-#define NEXT_XDP_SEND_BATCH_SIZE                                       32
+#define NEXT_XDP_SEND_BATCH_SIZE                                       64
 
 #else // #ifdef __linux__
 
