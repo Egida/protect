4551842bb879d12e199f49d37e19de51f72c1c45
Glenn Fiedler <glenn@networknext.com>
2025-11-26 17:17:54 -0500

    go

--- sdk/next_constants.h
+++ sdk/next_constants.h
@@ -21,7 +21,7 @@
 #define NEXT_XDP_SEND_QUEUE_SIZE                                     8192
 #define NEXT_XDP_RECV_QUEUE_SIZE                                     8192
 #define NEXT_XDP_FILL_QUEUE_SIZE                                     2048
-#define NEXT_XDP_SEND_BATCH_SIZE                                       64
+#define NEXT_XDP_SEND_BATCH_SIZE                                       32
 
 #else // #ifdef __linux__
 
