03ee2d3dee79c02a9a95c50b6fcfa17bfa83b73d
Glenn Fiedler <glenn@mas-bandwidth.com>
2025-11-28 11:40:45 -0500

    lock free receive double buffer

--- sdk/next_server_linux.cpp
+++ sdk/next_server_linux.cpp
@@ -88,8 +88,8 @@ struct next_server_xdp_socket_t
     uint32_t num_free_receive_frames;
     uint64_t receive_frames[NEXT_XDP_NUM_FRAMES/2];
     next_platform_thread_t * receive_thread;
-    std::atomic<uint64_t> send_counter_main_thread;
-    std::atomic<uint64_t> send_counter_send_thread;
+    std::atomic<uint64_t> receive_counter_main_thread;
+    std::atomic<uint64_t> receive_counter_send_thread;
     struct next_server_xdp_receive_buffer_t receive_buffer[2];
 
     uint8_t padding_3[1024];
